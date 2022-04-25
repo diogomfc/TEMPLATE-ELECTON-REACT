45d3d33743ccac3b8c23de92de7b2f067e357dd8
diogomfc <diogomfc@hotmail.com>
2022-04-25 08:36:14 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './pages/Header';
+
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
